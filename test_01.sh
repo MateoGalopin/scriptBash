14b665fd670e5b06a8f82116f8c7c596438a1713
mateo <galopinmateo47@gmail.com>
2024-01-24 10:08:52 +0100

    test

--- test_01.sh
+++ test_01.sh
@@ -5,3 +5,10 @@ age=30
 echo "$hello $name, tu as $age ans"
 age=$((age + 1))
 echo "$hello $name, tu as $age ans"
+
+poids=60
+taille=167
+
+res=echo "scale=2; taille/poids" | bc
+
+echo "ratio : $res"
